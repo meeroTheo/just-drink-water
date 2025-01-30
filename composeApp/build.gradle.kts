beba93eaf9162282a116d2b080d2dd4641195b2a
meeroTheo <44076238+meeroTheo@users.noreply.github.com>
2025-01-29 22:01:44 -0500

    feat: add navigation dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,13 +37,20 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.navigator)
+            implementation(libs.navigator.screen.model)
+            implementation(libs.navigator.transitions)
+            implementation(libs.navigator.koin)
+            implementation(libs.koin.core)
         }
+
     }
 }
 
--- composeApp/src/commonMain/kotlin/com/jdw/todo/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.jdw.todo
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
